92f0ecfe1afd76395be93ebe97af7a782bfe8037
Jon Solera <git@emufarm.org>
2023-02-21 17:42:25 -0800

    bug fixes

--- src/main/java/frc/robot/subsystems/BinarySensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/BinarySensorSubsystem.java
@@ -4,10 +4,8 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.DataLogManager;
-import edu.wpi.first.wpilibj.DigitalInput
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
